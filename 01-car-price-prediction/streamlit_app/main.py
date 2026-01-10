f0c4e205b86e95fa2049991a1a80ec14ade35067
nirmal2003-2 <nirmalkr360@gmail.com>
2026-01-11 00:37:44 +0530

    adjusted forced css styling para in streamlit code

--- 01-car-price-prediction/streamlit_app/main.py
+++ 01-car-price-prediction/streamlit_app/main.py
@@ -41,13 +41,13 @@ label {
     text-shadow: 1px 1px 4px rgba(0,0,0,0.9);
 }
 input, select {
-    background-color: rgba(20, 20, 20, 0.8) !important;
+    background-color: rgba(20, 20, 20, 0.85) !important;
     color: #ffffff !important;
     font-weight: 600 !important;
-    border-radius: 8px !important;
+    border-radius: 7px !important;
 }
 div[data-baseweb="select"] > div {
-    background-color: rgba(20, 20, 20, 0.8) !important;
+    background-color: rgba(20, 20, 20, 0.85) !important;
     color: white !important;
 }
 button[kind="primary"] {
